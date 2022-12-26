edb83836de721ec7b215d77216fecd7076fb1aeb
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-12-26 17:15:44 +0700

    add class node

--- BinarySearchTree_009/Program.cs
+++ BinarySearchTree_009/Program.cs
@@ -8,6 +8,16 @@ namespace BinarySearchTree_009
 {
     class Program
     {
+        /* A node class cons1 */
+
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+
+            //constructor for the node class
+        }
         static void Main(string[] args)
         {
         }

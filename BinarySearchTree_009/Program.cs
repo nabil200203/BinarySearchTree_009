8c3ad4b8699c40bd6fa5a3f823d4eace48a00680
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-12-26 18:17:02 +0700

    add  finish

--- BinarySearchTree_009/Program.cs
+++ BinarySearchTree_009/Program.cs
@@ -1,25 +1,172 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinarySearchTree_009
+namespace BinarySearchTree
 {
-    class Program
+    class node
     {
-        /* A node class cons1 */
+        public string info;
+        public node lchild;
+        public node rchild;
 
-        class node
+        //constructor for the node class
+
+        public node(string i, node l, node r)
         {
-            public string info;
-            public node lchild;
-            public node rchild;
+            info = i;
+            lchild = l;
+            rchild = r;
 
-            //constructor for the node class
+
+        }
+    }
+    class BinaryTree
+    {
+        public node ROOT;
+
+        public BinaryTree()
+        {
+            ROOT = null; // intializing root to null
+        }
+        public void insert(string element)// insert a node in the binary search trr
+        {
+            node tmp, parent = null, currentnode = null;
+            find(element, ref parent, ref currentnode);
+            if (currentnode != null)//
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else //if the specified node is not present
+            {
+                tmp = new node(element, null, null);
+                if (parent == null)
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    if (string.Compare(element, parent.info) < 0)
+                        parent.lchild = tmp;
+                }
+                else
+                {
+                    parent.rchild = tmp;
+                }
+            }
+        }
+        public void find(string element, ref node parent, ref node currentnode)
+        {
+
+            currentnode = ROOT;
+            parent = null;
+            while ((currentnode != null) && (currentnode.info != element))
+            {
+                parent = currentnode;
+                if (string.Compare(element, currentnode.info) < 0)
+                    currentnode = currentnode.lchild;
+                else
+                    currentnode = currentnode.rchild;
+            }
+        }
+
+        public void inorder(node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.lchild);
+                Console.WriteLine(ptr.info + "");
+                inorder(ptr.rchild);
+            }
+
+        }
+
+        public void preorder(node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + " ");
+                preorder(ptr.lchild);
+                preorder(ptr.rchild);
+            }
+        }
+
+        public void postorder(node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.lchild);
+                postorder(ptr.rchild);
+                Console.Write(ptr.info + " ");
+            }
         }
         static void Main(string[] args)
         {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform peorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nEnter your choice (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("invalid option");
+                            break;
+                        }
+                }
+            }
         }
     }
 }
